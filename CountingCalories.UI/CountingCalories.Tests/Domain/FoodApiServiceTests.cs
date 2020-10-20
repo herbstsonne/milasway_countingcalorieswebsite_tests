71c4000694f4907beb8ecda2adb5c8bb2f27625f
Mila <57719825+herbstsonne@users.noreply.github.com>
2020-10-20 18:47:16 +0200

    Service tests

---
+++ CountingCalories.UI/CountingCalories.Tests/Domain/FoodApiServiceTests.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
+using CountingCalories.Domain.Entities;
+using CountingCalories.Domain.Repository.Contract;
+using CountingCalories.Domain.Services;
+using CountingCalories.Shared.ViewModels;
+using Moq;
+using Xunit;
+
+namespace CountingCalories.Tests
+{
+    public class FoodApiServiceTests
+    {
+        private Mock<IAsyncRepository<FoodEntity>> _repoMoq;
+        private FoodApiService _service;
+
+        public FoodApiServiceTests()
+        {
+            SetupMocks();
+        }
+
+        private void SetupMocks()
+        {
+            _repoMoq = new Mock<IAsyncRepository<FoodEntity>>();
+
+            _repoMoq.Setup(r => r.GetAll())
+                .Returns(
+                new List<FoodEntity>()
+                {
+                    new FoodEntity{ Id = 0, Name = "Salad", CaloriesPer100G = 20},
+                    new FoodEntity{ Id = 1, Name = "Bread", CaloriesPer100G = 300}
+                }
+            );
+            _repoMoq.Setup(r => r.GetByName(It.IsAny<string>()))
+                .Returns(
+                    new FoodEntity { Id = 0, Name = "Salad", CaloriesPer100G = 20 }
+                );
+            var repo = _repoMoq.Object;
+            _service = new FoodApiService(repo);
+
+        }
+
+        [Fact]
+        public void TestGetAllFood()
+        {
+            var allFood = _service.GetAllFood();
+            var allFoodList = allFood.ToList();
+            Assert.NotNull(allFood);
+            Assert.Equal(2, allFoodList.Count());
+            var firstElement = allFoodList[0];
+
+            Assert.Equal(typeof(FoodView), firstElement.GetType());
+        }
+
+        [Fact]
+        public void GetFoodByName()
+        {
+            var foodView = _service.GetFoodByName("Bread");
+
+            Assert.NotNull(foodView);
+            Assert.Equal(typeof(FoodView), foodView.GetType());
+        }
+    }
+}
---
+++ CountingCalories.UI/CountingCalories.Tests/Domain/FoodPerDayApiServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using CountingCalories.Domain.Entities;
+using CountingCalories.Domain.Repository.Contract;
+using CountingCalories.Domain.Services;
+using CountingCalories.Shared.ViewModels;
+using Moq;
+using Xunit;
+
+namespace CountingCalories.Tests.Domain
+{
+    public class FoodPerDayApiServiceTests
+    {
+        private FoodPerDayApiService _service;
+
+        public FoodPerDayApiServiceTests()
+        {
+            SetupMocks();
+        }
+
+        private void SetupMocks()
+        {
+            var repoMoq = new Mock<IAsyncRepository<FoodPerDayEntity>>();
+
+            repoMoq.Setup(r => r.GetByDate(It.IsAny<string>()))
+                .Returns(
+
+                    new FoodPerDayEntity
+                    {
+                        Id = 0,
+                        Day = "01.10.2020",
+                        AllEntries = new List<FoodEntryEntity>()
+                        {
+                            new FoodEntryEntity
+                            {
+                                Id = 0,
+                                FoodId = 2,
+                                FoodName = "Bread",
+                                Amount = 20,
+                                Calories = 80
+                            }
+                        }
+                    }
+
+                );
+            _service = new FoodPerDayApiService(repoMoq.Object);
+        }
+
+        [Fact]
+        public void GetFoodPerDayByDateTest()
+        {
+            var date = "01.10.2020";
+            var res = _service.GetFoodPerDayByDate(date);
+            Assert.NotNull(res);
+
+            Assert.Equal(typeof(FoodPerDayView), res.GetType());
+
+            Assert.Equal(0, res.Id);
+            Assert.Equal(date, res.Day);
+            Assert.NotNull(res.AllFoodEntries);
+
+            var entries = res.AllFoodEntries;
+            Assert.Single(entries);
+
+            var firstEntry = entries[0];
+
+            Assert.Equal(2, firstEntry.FoodId);
+            Assert.Equal("Bread", firstEntry.FoodName);
+            Assert.Equal(20, firstEntry.Amount);
+            Assert.Equal(80, firstEntry.Calories);
+        }
+
+    }
+}
